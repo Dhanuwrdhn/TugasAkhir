4fdbcf0245ed52990cc2f269b603312308df9231
Dhanuwrdhn <dhanuwardhan09@gmail.com>
2023-05-09 13:17:05 +0700

    new branch

--- app/src/main/java/com/id/syahrial/hydroapp/home/MainActivity.kt
+++ app/src/main/java/com/id/syahrial/hydroapp/home/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.id.syahrial.hydroapp.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.controls.Control
 import android.view.WindowManager
 import com.id.syahrial.hydroapp.R
+import com.id.syahrial.hydroapp.control.ControlActivity
+import com.id.syahrial.hydroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Menghilangkan status bar
@@ -13,6 +18,11 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnControl.setOnClickListener {
+            val intent = Intent(this, ControlActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
